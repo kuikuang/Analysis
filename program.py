65126d7fe0a079fc9dbd35f57f509307a6fde075
kuikuang <3482671628@qq.com>
2019-04-30 09:01:44 +0800

    test

---
+++ program.py
@@ -0,0 +1,31 @@
+import dataProcess
+import  PCA
+import preProcess
+import IsolateForest
+HDFSfilePath='D:/data/test_result/struct_log.csv'
+labelFile='D:\data\HDFS\\anomaly_label.csv'
+trainRatio=0.5
+(x_train,y_train),(x_test,y_test)=dataProcess.HDFS(HDFSfilePath,trainRatio,labelFile)
+pre=preProcess.PreProcess()
+termWeighting='tf-idf'
+x_train=pre.fit_transform(x_train,termWeighting)
+x_test=pre.tranform(x_test,termWeighting)
+model=PCA.PCA()
+model.fit(x_train)
+print('Train validation:')
+precision,recall,f1=model.evaluate(x_train,y_train)
+print('Test validation:')
+precision, recall, f1 = model.evaluate(x_test, y_test)
+
+trainRatio=0.6
+(x_train1,y_train1),(x_test1,y_test1)=dataProcess.HDFS(HDFSfilePath,trainRatio,labelFile)
+pre=preProcess.PreProcess()
+termWeighting='None'
+x_train1=pre.fit_transform(x_train1,termWeighting)
+x_test1=pre.tranform(x_test1,termWeighting)
+model_iFrorest=IsolateForest.IsolationForest()
+model_iFrorest.fit(x_train1)
+print('Train validation:')
+precision_iForest_train,recall_iForest_train,f1_iForest_train=model_iFrorest.evaluate(x_train1,y_train1)
+print('Test validation:')
+precision_iForest_test, recall_iForest_test, f1_iForest_test = model_iFrorest.evaluate(x_test1, y_test1)
