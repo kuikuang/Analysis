3be3a66f2d0fa1a0d736ffb1c6762e5fe87f9556
kuikuang <3482671628@qq.com>
2019-05-19 09:55:23 +0800

    修改

--- LR.py
+++ LR.py
@@ -1,17 +1,21 @@
 import numpy as np
-from sklearn.ensemble import IsolationForest as iForest
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import precision_score, recall_score, f1_score
-class IsolationForest(iForest):
-    def _init_(self,n_estimators=100,max_samples='auto',contamination=0.03,**kwargs):
-        super(IsolationForest, self).__init__(n_estimators=n_estimators, max_samples=max_samples,
-                                              contamination=contamination, behaviour="new", **kwargs)
-    def fit(self,X):
+
+class LR(object):
+
+    def __init__(self, penalty='l2', C=100, tol=0.01, class_weight=None, max_iter=100):
+        self.classifier = LogisticRegression(penalty=penalty, C=C, tol=tol, class_weight=class_weight,
+                                             max_iter=max_iter)
+
+    def fit(self, X, y):
+
         print('====== Model summary ======')
-        super(IsolationForest, self).fit(X)
+        self.classifier.fit(X, y)
 
     def predict(self, X):
-        y_pred = super(IsolationForest, self).predict(X)
-        y_pred = np.where(y_pred > 0, 0, 1)
+
+        y_pred = self.classifier.predict(X)
         return y_pred
 
     def evaluate(self, X, y_true):
@@ -22,8 +26,3 @@ class IsolationForest(iForest):
         f1 = f1_score(y_true, y_pred)
         print('Precision: {:.3f}, recall: {:.3f}, F1-measure: {:.3f}\n'.format(precision, recall, f1))
         return precision, recall, f1
-
-
-
-
-
--- program.py
+++ program.py
@@ -1,31 +1,35 @@
 import dataProcess
 import  PCA
 import preProcess
-import IsolateForest
+import LR
 HDFSfilePath='D:/data/test_result/struct_log.csv'
 labelFile='D:\data\HDFS\\anomaly_label.csv'
-trainRatio=0.5
-(x_train,y_train),(x_test,y_test)=dataProcess.HDFS(HDFSfilePath,trainRatio,labelFile)
+'''trainRatio=0.5
+(x_train,y_train),(x_test,y_test),(blk_id_train,blk_id_test)=dataProcess.HDFS(HDFSfilePath,trainRatio,labelFile)
+
 pre=preProcess.PreProcess()
 termWeighting='tf-idf'
 x_train=pre.fit_transform(x_train,termWeighting)
 x_test=pre.tranform(x_test,termWeighting)
+
 model=PCA.PCA()
 model.fit(x_train)
 print('Train validation:')
-precision,recall,f1=model.evaluate(x_train,y_train)
+precision,recall,f1=model.evaluate(x_train,y_train,blk_id_train)
 print('Test validation:')
-precision, recall, f1 = model.evaluate(x_test, y_test)
+precision, recall, f1 = model.evaluate(x_test, y_test,blk_id_test)
+'''
 
-trainRatio=0.6
-(x_train1,y_train1),(x_test1,y_test1)=dataProcess.HDFS(HDFSfilePath,trainRatio,labelFile)
+trainRatio=0.5
+(x_train1,y_train1),(x_test1,y_test1),(blk_id_train,blk_id_test)=dataProcess.HDFS(HDFSfilePath,trainRatio,labelFile)
 pre=preProcess.PreProcess()
-termWeighting='None'
+termWeighting='tf-idf'
 x_train1=pre.fit_transform(x_train1,termWeighting)
 x_test1=pre.tranform(x_test1,termWeighting)
-model_iFrorest=IsolateForest.IsolationForest()
-model_iFrorest.fit(x_train1)
+model_LR=LR.LR()
+model_LR.fit(x_train1,y_train1)
 print('Train validation:')
-precision_iForest_train,recall_iForest_train,f1_iForest_train=model_iFrorest.evaluate(x_train1,y_train1)
+precision_LR_train,recall_iForest_train,f1_iForest_train=model_LR.evaluate(x_train1,y_train1)
 print('Test validation:')
-precision_iForest_test, recall_iForest_test, f1_iForest_test = model_iFrorest.evaluate(x_test1, y_test1)
+precision_LR_test, recall_iForest_test, f1_iForest_test = model_LR.evaluate(x_test1, y_test1)
+
